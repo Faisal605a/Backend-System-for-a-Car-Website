1f189c04246b3e3c749aea6e0ffe8cbccd0238a0
faalkanhal <FAALKANHAL@elm.sa>
2023-05-31 14:15:03 +0300

    First Commit

---
+++ boogle-maps/src/test/java/com/udacity/boogle/maps/BoogleMapsApplicationTests.java
@@ -0,0 +1,49 @@
+package com.udacity.boogle.maps;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.net.URI;
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class BoogleMapsApplicationTests {
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private JacksonTester<Address> json;
+//	@Test
+//	public void contextLoads() {
+//	}
+
+
+	@Test
+	public void getAddress() throws Exception {
+
+		ResultActions resultActions =  mvc.perform(
+						get(new URI("/maps"))
+								.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+
+	}
+}
